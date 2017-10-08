c7b4d07420de831c00889620922d2befcc06c1b6
ningyuwen <1052447935@qq.com>
2017-10-08 23:11:40 +0800

    解决启动页白屏问题

---
+++ app/src/main/java/com/example/ningyuwen/music/view/activity/impl/StartPageActivity.java
@@ -0,0 +1,20 @@
+package com.example.ningyuwen.music.view.activity.impl;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * 启动页
+ * Created by ningyuwen on 17-10-8.
+ */
+
+public class StartPageActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        startActivity(new Intent(StartPageActivity.this, MainActivity.class));
+    }
+}
