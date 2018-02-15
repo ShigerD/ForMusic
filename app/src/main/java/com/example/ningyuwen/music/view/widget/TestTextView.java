46e5e844472288733494e89f3b9979a534b7c9d6
ningyuwen <1052447935@qq.com>
2018-02-15 10:50:57 +0800

    修复一些闪退问题

---
+++ app/src/main/java/com/example/ningyuwen/music/view/widget/TestTextView.java
@@ -0,0 +1,48 @@
+package com.example.ningyuwen.music.view.widget;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.support.annotation.Nullable;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.Scroller;
+import android.widget.TextView;
+
+/**
+ * Created by adu on 18-2-8.
+ */
+
+public class TestTextView extends View {
+    public TestTextView(Context context) {
+        super(context);
+    }
+
+    public TestTextView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public TestTextView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+
+        Scroller scroller = new Scroller(context);
+        invalidate();
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        super.onLayout(changed, left, top, right, bottom);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+    }
+
+
+}
